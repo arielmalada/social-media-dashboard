a8a644a199321a4177964b4f443adaf78e91d359
ariel.malada <ariel.malada@hukumonline.com>
2021-06-05 20:53:53 +0700

    add card user post

--- src/components/detailUser.js
+++ src/components/detailUser.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
+import { Card, CardBody, CardText, CardTitle, Col, Container, Row } from "reactstrap";
 import { fetchUserDetail } from "../store/actions/userDetail";
 
 const DetailUser = () => {
@@ -9,22 +10,44 @@ const DetailUser = () => {
   const userDetail = useSelector(
     (state) => state.userDetail.data
   );
-  
+
   useEffect(() => {
     if (userDetail) setUser(userDetail);
   }, [userDetail]);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(fetchUserDetail(userId));
-  }, [dispatch,userId]);
-  
-  const { name, username } = {...userData};
-  return ( 
-    <div>
-      {name} ( {username} )
-    </div>
-   );
+  }, [dispatch, userId]);
+
+  const { name = '', username = '', posts = [] } = { ...userData };
+  return (
+    <Container>
+      <div >
+        {name} ( {username} )
+      </div>
+      <Row>
+        <Col xs={12} md={5}>
+          sidebar
+        </Col>
+        <Col>
+          {
+            posts.map((post) => {
+              const { id, title } = post;
+              return (
+                <Card key={id} className="my-2">
+                  <CardBody>
+                    <CardTitle>{name}</CardTitle>
+                    <CardText>{title}</CardText>
+                  </CardBody>
+                </Card>
+              )
+            })
+          }
+        </Col>
+      </Row>
+    </Container>
+  );
 }
- 
+
 export default DetailUser;
