7c1e5d4abb9773fff380cc4af9072a92455408eb
ariel.malada <ariel.malada@hukumonline.com>
2021-06-05 20:53:30 +0700

    add more endpoint to get more user data for fetchUserDetail

--- src/store/actions/userDetail.js
+++ src/store/actions/userDetail.js
@@ -1,14 +1,26 @@
+import { getAlbums } from "../../services/albums";
+import { getPost } from "../../services/post";
 import { getUserDetail } from "../../services/users";
 
 export const getUserDetailAction = (data) => {
   return { type: 'GET_USER_DETAIL', data: data };
 };
 
-export const fetchUserDetail = (id) => {
+export const fetchUserDetail =  (id) => {
   return (dispatch) => {
-    getUserDetail(id).then(
+    const userData = async (id) => await Promise.all([
+      getUserDetail(id),
+      getPost(id),
+      getAlbums(id)
+    ]);
+    userData(id).then(
       (res) => {
-        return dispatch(getUserDetailAction(res.data));
+        console.log(res);
+        return dispatch(getUserDetailAction({
+          ...res[0].data,
+          posts: [...res[1].data],
+          albums: [...res[2].data]
+        }));
       }
     );
   }
