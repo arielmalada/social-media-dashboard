c7bcd6517d0eb7f15d5d78e6d394ab3b521b313e
ariel.malada <ariel.malada@hukumonline.com>
2021-06-06 08:06:47 +0700

    add edit button, & photo profile on card

--- src/components/detailUser.js
+++ src/components/detailUser.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
-import { Card, CardBody, CardLink, CardText, CardTitle, Col, Container, Row } from "reactstrap";
+import { Card, CardBody, CardLink, CardText, CardTitle, Col, Container, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledDropdown } from "reactstrap";
 import { fetchUserDetail } from "../store/actions/userDetail";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAt, faGlobe, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faAt, faGlobe, faChevronRight, faUserFriends, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import Avatar from "react-avatar";
 
 const DetailUser = () => {
@@ -35,7 +35,7 @@ const DetailUser = () => {
     <Container>
       <div className="text-center">
         <div>
-          <Avatar name={name} round/>
+          <Avatar name={name} round />
         </div>
         {name} ( {username} )
       </div>
@@ -57,7 +57,7 @@ const DetailUser = () => {
                     const { id, title } = album
                     return (
                       <Col xs={4} key={id} className="text-truncate text-center mb-2">
-                        <Avatar name={title}/>
+                        <Avatar name={title} />
                       </Col>
                     )
                   })
@@ -73,9 +73,25 @@ const DetailUser = () => {
               return (
                 <Card key={id} className="my-2">
                   <CardBody>
-                    <CardTitle>{name}</CardTitle>
+                    <div className="float-right">
+                      <UncontrolledDropdown>
+                        <DropdownToggle color="link">
+                          <FontAwesomeIcon icon={faEllipsisV} size="xs" />
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                          <DropdownItem>Edit</DropdownItem>
+                        </DropdownMenu>
+                      </UncontrolledDropdown>
+                    </div>
+                    <CardTitle className="d-flex">
+                      <Avatar name={name} round className="mr-2" size="48" />
+                      <span>
+                        <div>{name}</div>
+                        <div><FontAwesomeIcon icon={faUserFriends} size="xs" /></div>
+                      </span>
+                    </CardTitle>
                     <CardText>{title}</CardText>
-                    <CardLink href="#">Comments</CardLink>
+                    <CardLink href="#" className="float-right">Comments</CardLink>
                   </CardBody>
                 </Card>
               )
