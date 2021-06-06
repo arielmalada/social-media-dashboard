7ba12eddf6c586c9b2622ff29459d0f2b4014ebc
ariel.malada <ariel.malada@hukumonline.com>
2021-06-06 09:42:46 +0700

    form add post 80%

--- src/components/detailUser.js
+++ src/components/detailUser.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
-import { Card, CardBody, CardLink, CardText, CardTitle, Col, Container, DropdownItem, DropdownMenu, DropdownToggle, Row, UncontrolledDropdown } from "reactstrap";
+import { Card, CardBody, CardLink, CardText, CardTitle, Col, Container, DropdownItem, DropdownMenu, DropdownToggle, Form, FormGroup, Input, Row, UncontrolledDropdown } from "reactstrap";
 import { fetchUserDetail } from "../store/actions/userDetail";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAt, faGlobe, faChevronRight, faUserFriends, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 import Avatar from "react-avatar";
+import { useForm } from "react-hook-form";
+import { Button } from "bootstrap";
 
 const DetailUser = () => {
   const { userId } = useParams();
   const [userData, setUser] = useState(null);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
   const userDetail = useSelector(
     (state) => state.userDetail.data
   );
@@ -31,6 +37,7 @@ const DetailUser = () => {
     email = '',
     website = ''
   } = { ...userData };
+  const reversedPost = [...posts].reverse();
   return (
     <Container>
       <div className="text-center">
@@ -67,9 +74,25 @@ const DetailUser = () => {
           </Card>
         </Col>
         <Col>
+          <Card>
+            <CardBody>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="d-flex flex-row mb-3 align-items-center">
+                  <div>
+                    <Avatar name={name} round className="mr-2" size="48" />
+                  </div>
+                  <div  className="w-100">
+                    <input {...register("title")} className="form-control mb-2" placeholder="title" />
+                    <input {...register("body")} className="form-control" placeholder="what's on your mind" />
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Post</button>
+              </form>
+            </CardBody>
+          </Card>
           {
-            posts.map((post) => {
-              const { id, title } = post;
+            reversedPost.map((post) => {
+              const { id, title, body } = post;
               return (
                 <Card key={id} className="my-2">
                   <CardBody>
@@ -91,6 +114,7 @@ const DetailUser = () => {
                       </span>
                     </CardTitle>
                     <CardText>{title}</CardText>
+                    <CardText>{body}</CardText>
                     <CardLink href="#" className="float-right">Comments</CardLink>
                   </CardBody>
                 </Card>
