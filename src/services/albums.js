a0b5cdf6ee1ea1cf4d7d91370c37924a04d7440b
ariel.malada <ariel.malada@hukumonline.com>
2021-06-06 23:15:26 +0700

    add album detail endpoint

--- src/services/albums.js
+++ src/services/albums.js
@@ -9,7 +9,17 @@ export const getAlbums = async (id) => {
   }
 }
 
-export const getPhotos = async (id) => {
+export const getAlbumsDetail = async (id) => {
+  try {
+    const res = await get(`albums/${id}`);
+    console.log(res);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getAlbumPhotos = async (id) => {
   try {
     const res = await get(`albums/${id}/photos`);
     return res;
